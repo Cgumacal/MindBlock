174ae1c0b38dbf0d6b12c4767a37ad640ad45350
Erleh <eric.lee0051@gmail.com>
2017-05-30 20:18:44 -0700

    Adds block designs and a level design
    Blocks added - Popup Block and sequence blocks script
    Level added - level 1 test
    scripts related to added blocks also added

---
+++ Assets/Scripts/ericL_BlockInSequence.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ericL_BlockInSequence : MonoBehaviour {
+
+	//INSTRUCTIONS:   place the SequenceBlocks.cs script onto the block to manage the 'key' blocks to be placed in a sequence
+	//                you will be able to manage the inspector from the block with the SequenceBlocks.cs script
+
+	//                BlockInSequence.cs is to be placed on the TRIGGER of a block
+	//                blocks with the BlockInSequences.cs script on them will be placed in the order they are entered into the SequenceBlocks.cs inspector
+	//                this order will be how to unlock the 'completed' task
+
+	//                the reward for completing a task will be from ANY gameobject's script that has a function name of 'reward'
+	//                the gameobject containing the script with 'reward' must be placed within the SequenceBlocks.cs inspector
+
+	//                the 'reward' function is called throught the function <gameObjectName>.SendMessage(" nameofmethod ");
+
+
+	public bool isTriggered = false;
+
+	public ericL_SequenceBlocks testOrder;
+
+	//Creates order of sequence from sibling script
+	void Start()
+	{
+		//Debug.Log ("tested");
+		//testOrder.createOrder ();
+	}
+
+	//Activates the item to begin sequence
+	void OnTriggerEnter()
+	{
+		isTriggered = true;
+		//Tests trigger order
+		testOrder.testOrder ();
+	}
+
+	//Changes the value of item to activate to false (to reset search for sequence)
+	public void wrongOrder()
+	{
+		isTriggered = false;
+	}
+}
---
+++ Assets/Scripts/ericL_PopoutBlock.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ericL_PopoutBlock : MonoBehaviour {
+
+	//FYI : AUTOSNAP FEATURE OF THE MAP EDITOR WILL MOVE THE TRIGGER FOR THIS OBJECT INSIDE OF THE BLOCK ITSELF, MAKING IT UNREACHABLE
+	//      SO IF THAT HAPPENS, MAKE SURE TO MOVE IT BACK INTO A REACHABLE PLACEMENT, IT IS LABELED AS THE TRIGGER
+
+
+	//INSTRUCTIONS -         once the Popoutblock.cs script is placed onto the TRIGGER of an object, place the block you wish to be spawned in
+	//                       dispensedBlock,   place a gameObject without a renderer to use for its transform location in slots created under
+	//                       spawnPoints,      edit the duration value to increase or decrease the duration of spawned blocks before despawn
+
+	//                       not all blocks have been tested with use of PopoutBlock, there may still be bugs
+
+	//Declare what block type to spawn in inspector
+	public GameObject dispensedBlock;
+	//Declare spawn points of blocks to be spawned in inspector by creating spawn point objects in the game screen and placing in the inspector
+	public GameObject[] spawnPoints;
+	//Time before objects are deleted from creation
+	public float duration = 5f;
+	//To temporarilly hold the block to be instantiated / created into the game screen
+	private List<GameObject> summonedBlock = new List<GameObject>();
+
+	bool isColliding = false;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (isColliding)
+			return;
+		isColliding = true;
+		Invoke ("spawnBlocks", 0);
+	}
+
+	void spawnBlocks()
+	{
+		foreach (GameObject i in spawnPoints) {
+			var newBlock = ((GameObject)Instantiate (dispensedBlock, i.transform.position, i.transform.rotation));
+			summonedBlock.Add(newBlock);
+			newBlock.transform.parent = i.transform;
+
+			Invoke ("deleteBlocks", duration);
+			//Destroy (summonedBlock, duration);
+		}
+	}
+	//To delete block object without deleting player on top of block
+	void deleteBlocks()
+	{
+		Transform tempPlayer = findPlayer ();
+
+		if (tempPlayer != null) {
+			if (tempPlayer.name == "Player") {
+				Debug.Log ("DEPARENTED");
+				tempPlayer.parent = null;
+			}
+		}
+
+		foreach (GameObject i in summonedBlock) {
+			Destroy (i);
+		}
+		summonedBlock.Clear ();
+	}
+
+	Transform findPlayer()
+	{
+		foreach (GameObject i in summonedBlock) {
+			if (i.transform.Find ("Player") != null) {
+				return i.transform.Find ("Player");
+			}
+		}
+
+		return null;
+	}
+}
