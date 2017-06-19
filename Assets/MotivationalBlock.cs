626549bf2cfd615993be5a6d2b6fd8fcc7119775
Cgumacal <chris.gumacal94@gmail.com>
2017-06-18 20:26:47 -0700

    Motivational Block implementation
    Added in the motivational block to the game including prefab.
    
    within 3 units of the block you will be able to teleport an aditional 3
    units distance away
    
    Also refactored the teleport distance to be a variable in the
    FPScontroller script so it can be easily changed

---
+++ Assets/MotivationalBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotivationalBlock : MonoBehaviour {
+    private GameObject player;
+    private FPScontroller controller;
+    [SerializeField] private float AreaOfEffect = 3f;
+    [SerializeField] private float increasedTeleportRange = 3f;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        controller = player.GetComponent<FPScontroller>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(player.transform.position,transform.position) <= AreaOfEffect)
+        {
+            controller.setMaxTeleport(controller.defaultMaxTeleport + increasedTeleportRange);
+        }
+        else
+        {
+            controller.resetTeleportDistance();
+        }
+	}
+}
